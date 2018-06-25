76fc82373bbf8180714102684be8d96b897d1746
Hiki <aneureka2@gmail.com>
2018-06-25 20:41:24 +0800

    加入紫书

---
+++ purple-book-exercise/cmake-build-debug/CMakeFiles/purple_book_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/purple_book_exercise.dir/main.cpp.o"
+  "purple_book_exercise.pdb"
+  "purple_book_exercise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/purple_book_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ purple-book-exercise/cmake-build-debug/CMakeFiles/purple_book_exercise.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for purple_book_exercise.
+# This may be replaced when dependencies are built.
